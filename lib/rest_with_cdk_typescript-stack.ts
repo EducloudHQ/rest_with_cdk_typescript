ef4c2d79c1a4e1c47a4a5e6c6041d52dcd967b06
vernyuy <fonchu.e.venyuy@gmail.com>
2023-07-07 10:01:21 +0100

    get

--- lib/rest_with_cdk_typescript-stack.ts
+++ lib/rest_with_cdk_typescript-stack.ts
@@ -17,28 +17,8 @@ export class RestWithCdkTypescriptStack extends Stack {
       tableName:"cdkTypescript1",
       partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
     });
-///////////////////////////////////////////////////////////////////////////////////////////////////////
-    // LAmbda create resource
-
-    const create_weather_lambda = new lambda.Function(this, "CreateWeatherLambdaFunction",
-                                         {
-                                          functionName: 'cdk-typescript-create',
-                                          runtime:lambda.Runtime.NODEJS_14_X,
-                                          handler: 'createWeather.lambdaHandler',
-                                          code : lambda.Code.fromAsset('src'),
-                                          environment: { 
-                                              'TABLE_NAME': table.tableName
-                                          }
-                                        })
     // Api gateway resource
     const weather_api = new apigw.RestApi( this, 'weather_rest_api')
-    // Lambda permissions
-    table.grantReadWriteData(create_weather_lambda)
-                                  //  Create item route
-    weather_api.root.addResource("create-weather").addMethod(
-                                              "POST",
-                                              new apigw.LambdaIntegration(create_weather_lambda)
-                                          )
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -64,77 +44,6 @@ export class RestWithCdkTypescriptStack extends Stack {
             "GET",
             new apigw.LambdaIntegration(get_weather_lambda)
         )   
-      
-    //////////////////////////////////////////////////////////////////////////////
-
-     // LAmbda list resource
-
-     const list_weather_lambda = new lambda.Function(this, "listWeatherLambdaFunction",
-     {
-       functionName: 'cdk-typescript-list',
-       runtime:lambda.Runtime.NODEJS_14_X,
-       handler: 'listWeathers.lambdaHandler',
-       code : lambda.Code.fromAsset('src'),
-       environment: { 
-         'TABLE_NAME': table.tableName
-       }
-     })
- 
-     // Lambda permissions
-     table.grantReadData(list_weather_lambda)
-     //  list item route
-         weathers.addMethod(
-             "GET",
-             new apigw.LambdaIntegration(list_weather_lambda)
-         )
-
-
-         //////////////////////////////////////////////////////////////////////////////////////   
-        
-         // LAmbda delete resource
-
-         const delete_weather_lambda = new lambda.Function(this, "deleteWeatherLambdaFunction",
-         {
-           functionName: 'cdk-typescript-delete',
-           runtime:lambda.Runtime.NODEJS_14_X,
-           handler: 'deleteWeather.lambdaHandler',
-           code : lambda.Code.fromAsset('src'),
-           environment: { 
-             'TABLE_NAME': table.tableName
-           }
-         })
-     
-         // Lambda permissions
-         table.grantReadWriteData(delete_weather_lambda)
-         //  Create item route
-             weather.addMethod(
-                 "DELETE",
-                 new apigw.LambdaIntegration(delete_weather_lambda)
-             )   
-           
-             //////////////////////////////////////////////////////////////////////////////////////   
-        
-         // LAmbda update resource
-
-         const update_weather_lambda = new lambda.Function(this, "updateWeatherLambdaFunction",
-         {
-           functionName: 'cdk-typescript-update',
-           runtime:lambda.Runtime.NODEJS_14_X,
-           handler: 'updateWeather.lambdaHandler',
-           code : lambda.Code.fromAsset('src'),
-           environment: { 
-             'TABLE_NAME': table.tableName
-           }
-         })
-     
-         // Lambda permissions
-         table.grantReadWriteData(update_weather_lambda)
-         //  Create item route
-             weather.addMethod(
-                 "PUT",
-                 new apigw.LambdaIntegration(update_weather_lambda)
-             )   
-            
-    
+         
   }
 }
